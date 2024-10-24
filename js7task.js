58fa117a1b3998c2b147be0ebb035bafea1bf2d4
Srinivas996622 <chillimunthanagasaisrinivas@gmail.com>
2024-10-24 18:45:12 +0530

    jstqsk-7

---
+++ js7task.js
@@ -0,0 +1,46 @@
+//                                            TASK-1
+
+var light = "red";
+if(light == "red"){
+    console.log("Stop.");
+} else if(light == "yellow"){
+    console.log("Get ready to move.");
+} else if(light == "green"){
+    console.log("Go.");
+}
+else{
+    console.log("Invalid input.")
+}
+
+//                                            TASK-2
+var temp = 1;
+if(temp == 0){
+    console.log("It's freezing cold.");
+} else if(temp == 15){
+    console.log("It's a cool day.");
+} else if(temp == 25){
+    console.log("It's a pleasant day.")
+}
+else{
+    console.log("Unknown weather.")
+}
+//                                            TASK-3
+var num = 10;
+if(num == 1){
+    console.log("Today is Monday.");
+} else if(num == 2){
+    console.log("Today is Tuesday.");
+} else if(num == 3){
+    console.log("Today is Wednesday.");
+} else if(num == 4){
+    console.log("Today is Thursday.");
+} else if(num == 5){
+    console.log("Today is Fridayday.");
+} else if(num == 6){
+    console.log("Today is Saturdayday.");
+} else if(num == 7){
+    console.log("Today is Sundayday.");
+}
+else{
+    console.log("Invalid day number.")
+}
